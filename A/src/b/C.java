7c35527fe4f50c4140c6d5756c9532d67cded139
Piyush Verma <Piyush Verma@PiyushVerma>
2020-11-28 07:57:05 +0530

    first commit

---
+++ A/src/b/C.java
@@ -0,0 +1,7 @@
+package b;
+
+public class C {
+	public void msg() {
+		System.out.println("Hiii");
+	}
+}
---
+++ A/src/b/D.java
@@ -0,0 +1,10 @@
+package b;
+
+public class D {
+
+	public static void main(String str[]) {
+		C obj=new C();
+		obj.msg();
+	}
+
+}
